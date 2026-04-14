fe07947e810057667f1ddca492363b218938b69e
chottomatt <matheusrocha0110@gmail.com>
2026-04-13 23:49:16 -0300

    Update

--- aula37.py
+++ aula37.py
@@ -6,8 +6,21 @@ Loop infinitio -> Quando um código não tem fim
 """
 contador = 0
 
-while contador <= 100:
-    contador = contador + 1 
-    print(contador)
+# while contador <= 100:
+#     contador += 1
 
-print('Acabou')
+#     if contador == 6:
+#         print('Não vou mostrar o 6')
+#         continue
+
+#     if contador >= 10 and contador <= 27:
+#         print('Não vou mostrar o', contador)
+#         continue
+
+#     print(contador)
+
+#     if contador == 40:
+#         break
+
+# print('Acabou')
+
