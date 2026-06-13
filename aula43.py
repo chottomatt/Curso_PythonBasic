f4c21cbb04c9a843e186a02a230eec9030d3177f
chottomatt <matheusrocha0110@gmail.com>
2026-06-13 12:33:55 -0300

    aula43 atualizada

--- aula43.py
+++ aula43.py
@@ -1,9 +1,19 @@
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print(repeticoes)
+# print('Aquele laçõ acima pode ter repetições infinitas')
+
 texto = 'Python'
 
-i = 0
-tamanho_string = len(texto)
-
-while i < tamanho_string:
-    print(texto[i])
-
-    i += 1
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
