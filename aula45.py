c80b082ca7e487efe4974ac8a4941c5f397bd02c
chottomatt <matheusrocha0110@gmail.com>
2026-06-18 11:55:12 -0300

    Novas aulas

---
+++ aula45.py
@@ -0,0 +1,20 @@
+"""
+Iterável -> str, range, etc (tem um método "__iter__" dentro)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+
+# for letra in texto
+texto = 'Luiz' # iterável
+iterador = iter(texto) #iterator
+
+while True:
+    try:
+        letra= next(iterador)
+        print(letra)
+    except:
+        break
+
+"""Isso é um exemplo do que o loop 
+"for" faz por baixo dos panos"""
