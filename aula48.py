ab22fa93e3994d5f6de2f47de73ba7cdd5d4a7cd
chottomatt <matheusrocha0110@gmail.com>
2026-06-20 12:33:28 -0300

    Aula 49 adicionada

---
+++ aula48.py
@@ -0,0 +1,19 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+#        +01234
+#        -54321
+string = 'ABCDE' # 5 CARACTERES (len)
+
+#print(bool([])) # falsy
+# print(lista, type(lista))
+
+#        0    1      2              3   4
+lista = [123, True, 'Luiz Otávio', 1.2, []]
+lista [-3] = 'Matheus'
+print (lista[2], type(lista[2]))
